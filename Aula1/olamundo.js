879f5868523871bfd5abdc3122ba8ec0edca039f
matheus128xd <mth.fidelis@gmail.com>
2022-02-21 22:30:47 -0300

    Auka2

---
+++ Aula1/olamundo.js
@@ -0,0 +1,10 @@
+console.log('Olá mundo')
+let a = 2;
+console.log('Esse é o conteudo de a = ',a)
+let b = 3;
+console.log('Esse é o resultado de ', a , '+', b, '=', a+b)
+console.log('Esse é o resultado de ', a , '-', b, '=', a-b)
+console.log('Esse é o resultado de ', a , 'x', b, '=', a*b)
+console.log('Esse é o resultado de ', a , '/', b, '=', a/b)
+let nome = 'Fidelis'
+console.log('Meu nome é', nome)
